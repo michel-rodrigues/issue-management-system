840a8baaa112e7a57153fd14d417fe594a47e277
Michel Rodrigues <michel.rodrigues86@yahoo.com.br>
2019-05-23 00:03:21 -0300

    estrutura inicial para usar como guia de onde colocar o código

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='issue-management-system',
+    url='https://github.com/michel-rodrigues/issue-management-system',
+    packages=find_packages('.'),
+)
